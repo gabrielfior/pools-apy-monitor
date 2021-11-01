93a5d9a62981b839e447a364dcb1699e3262e664
Gabriel Fior <fior.gbm@gmail.com>
2021-11-01 17:09:12 +0100

    Added pycoingecko to requirements.txt

--- db/write_to_table.py
+++ db/write_to_table.py
@@ -20,7 +20,6 @@ class DBWriter:
 
     def __init__(self) -> None:
         DATABASE_URL = os.environ['DATABASE_URL'].replace('postgres', 'postgresql')
-        # DATABASE_URL = 'postgresql://qyhodlgosoxuwr:1e848439d1daa4c1f089c28de1118cd37ea4a3db03d9a3fe5f97bf94f750784a@ec2-176-34-116-203.eu-west-1.compute.amazonaws.com:5432/depbto19usunbp'
         self.engine = sqlalchemy.create_engine(DATABASE_URL)
         self.TABLENAME = 'apys'
 
